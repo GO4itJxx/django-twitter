eda18b6dca3cc7aafb6fc13b838764b24e8a0a00
Jingxi Xu <jingxi0529@gmail.com>
2023-07-10 11:49:51 -0500

    correct comments/admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,1 @@
-from django.contrib import admin
-from comments.models import Comment
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('tweet', 'user', 'content', 'created_at', 'updated_at')
-    date_hierarchy = 'created_at'
