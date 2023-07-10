84f9698a2a42b246798a30ea98947ecce0c49bcf
Jingxi Xu <jingxi0529@gmail.com>
2023-07-10 18:46:20 -0500

    create api for comments

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -16,6 +16,9 @@ class UserSerializerForTweet(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username')
 
+class UserSerializerForComment(UserSerializerForTweet):
+    pass
+
 class SignupSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=20, min_length=6)
     password = serializers.CharField(max_length=20, min_length=6)
